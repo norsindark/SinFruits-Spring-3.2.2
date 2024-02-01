faa59ff1019acd71bfae4e3fe141f923788d6f39
Nguyen Tuan <norsinlight@gmail.com>
2024-02-01 14:12:27 +0700

    Initialize the working environment

---
+++ src/main/java/com/sinfruits/SinfruitsApplication.java
@@ -0,0 +1,13 @@
+package com.sinfruits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinfruitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinfruitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinfruits/SinfruitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinfruits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinfruitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
